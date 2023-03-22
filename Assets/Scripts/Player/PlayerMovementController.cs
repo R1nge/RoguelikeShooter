e91441f1183f2fb7555a04362bb21737d762b723
R1nge <r1ngegamedev@gmail.com>
2023-03-22 09:12:49 +0300

    feat: double jump

--- Assets/Scripts/Player/PlayerMovementController.cs
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -9,9 +9,11 @@ namespace Player
         [SerializeField] private float runningSpeed = 11.5f;
         [SerializeField] private float jumpSpeed = 8f;
         [SerializeField] private float gravity = 20f;
+        [SerializeField] private int maxJumps;
         private CharacterController _characterController;
         private Vector3 _moveDirection = Vector3.zero;
         private bool _canMove = true;
+        private int _jumpCount;
 
         private void Awake()
         {
@@ -36,20 +38,32 @@ namespace Player
             float movementDirectionY = _moveDirection.y;
             _moveDirection = forward * curSpeedX + right * curSpeedY;
 
-            if (Input.GetButton("Jump") && _canMove && _characterController.isGrounded)
+            if (_characterController.isGrounded)
             {
-                _moveDirection.y = jumpSpeed;
+                _jumpCount = 0;
             }
             else
             {
-                _moveDirection.y = movementDirectionY;
+                movementDirectionY -= gravity;
             }
 
-            if (!_characterController.isGrounded)
+            if (Input.GetButtonDown("Jump"))
             {
-                _moveDirection.y -= gravity * Time.deltaTime;
+                if (_characterController.isGrounded)
+                {
+                    movementDirectionY = jumpSpeed;
+                }
+                else
+                {
+                    if (_jumpCount < maxJumps - 1)
+                    {
+                        _jumpCount++;
+                        movementDirectionY = jumpSpeed;
+                    }
+                }
             }
 
+            _moveDirection.y = movementDirectionY;
             _characterController.Move(_moveDirection * Time.deltaTime);
         }
     }
